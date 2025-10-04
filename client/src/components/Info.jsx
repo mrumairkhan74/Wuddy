dbad4611b0243f68d203bbea360bd6d6e630f17e
Umair Khan <mrumairkhan74@gmail.com>
2025-10-04 15:21:06 +0500

    Edit bio and details

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -10,7 +10,7 @@ const Info = () => {
   const { user, loading, error } = useSelector((state) => state.auth)
   const [info, setInfo] = useState(false);
   const [details, setDetails] = useState(false);
-  const [form, setForm] = useState({ bio: user?.bio || "", address: user?.address, city: user?.city, country: user?.country, phoneNo: user?.phoneNo, education: user?.education })
+  const [form, setForm] = useState({ bio: user?.bio || "", address: user?.address, city: user?.city, country: user?.country, phoneNo: user?.phoneNo, education: user?.education, zipCode: user?.zipCode })
 
   const handleUpdate = async (e) => {
     e.preventDefault();
@@ -18,16 +18,16 @@ const Info = () => {
     setInfo(false)
   }
 
-  const handleUpdateInfo = async (e) => {
+  const handleUpdateDetails = async (e) => {
     e.preventDefault();
-    await dispatch(updatedUser({ userId: user._id, updateData: { address: form.address, city: form.city, country: form.country, education: form.education } }))
+    await dispatch(updatedUser({ userId: user._id, updateData: { address: form.address, city: form.city, country: form.country, education: form.education, zipCode: form.zipCode } }))
     setDetails(false)
   }
 
   return (
     <>
       <div className="rounded-md bg-[#206059]/10 w-full flex-[1] p-5 mb-5">
-        <h6 className="font-bold font-[Poppins] p-3">Intro</h6>
+        <h6 className="font-bold font-[Poppins] p-3">Bio</h6>
 
         <p className="text-center p-5 text-xl">
           "{user?.bio}"
@@ -44,26 +44,26 @@ const Info = () => {
         <div className="m-2">
           <div className="p-2 flex items-center gap-2">
             <GiGraduateCap className="text-[#206059] text-2xl" />
-            <span>B.S. Software Engineering</span>
+            <span>{user?.education}</span>
           </div>
 
           <div className="p-2 flex items-center gap-2">
             <FaHome className="text-[#206059] text-2xl" />
             <span>
-              Lives in <span className="font-bold">Jand, Punjab, Pakistan</span>
+              Lives in <span className="font-bold">{user?.address} </span>
             </span>
           </div>
 
           <div className="p-2 flex items-center gap-2">
             <FaLocationDot className="text-[#206059] text-2xl" />
             <span>
-              From <span className="font-bold">Attock, Pakistan</span>
+              From <span className="font-bold">{user?.city},{user?.country}</span>
             </span>
           </div>
 
           <div className="p-2 flex items-center gap-2">
             <FaHeart className="text-[#206059] text-2xl" />
-            <span>Single</span>
+            <span>{user?.status}</span>
           </div>
 
           <div className="p-2 flex items-center gap-2">
@@ -110,18 +110,18 @@ const Info = () => {
       {/* details */}
       {details && (
         <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
-          <div className="bg-white w-[400px] md:w-[600px] h-[300px] overflow-y-auto md:h-[600px] rounded-md shadow-lg relative">
-            <div className="flex bg-[#206059] rounded-t-md p-5">
-              <h4 className="font-[Poppins] text-xl">Edit Details</h4>
+          <div className="bg-white w-[400px] md:w-[600px] h-[600px] md:h-[400px] overflow-y-auto rounded-md shadow-lg relative">
+            <div className="flex bg-[#206059] rounded-t-md p-5 sticky">
+              <h4 className="font-[Poppins] text-xl text-white tracking-wide">Edit Details</h4>
               <button
-                onClick={() => setInfo(false)}
+                onClick={() => setDetails(false)}
                 className="absolute top-2 right-2 text-white hover:text-black"
               >
                 ✕
               </button>
             </div>
 
-
+            {/* form details */}
 
             <div className="flex items-center justify-center">
               <form className="flex flex-col items-center justify-center p-5">
@@ -129,18 +129,34 @@ const Info = () => {
                   <div className="flex flex-col md:flex-row gap-2">
                     <div className="flex-col gap-2 items-center">
                       <label htmlFor="address">Address:</label>
-                      <input type="text" className="bg-white w-full border p-2 rounded-md" />
+                      <input type="text" className="bg-white w-full border p-2 rounded-md" placeholder={`${user?.address}`} value={form.address} onChange={(e) => setForm({ ...form, address: e.target.value })} />
                     </div>
                     <div className="flex-col gap-2 items-center">
-                      <label htmlFor="address">City:</label>
-                      <input type="text" className="bg-white w-full border p-2 rounded-md" />
+                      <label htmlFor="city">City:</label>
+                      <input type="text" className="bg-white w-full border p-2 rounded-md" placeholder={`${user?.city}`} value={form.city} onChange={(e) => setForm({ ...form, city: e.target.value })} />
                     </div>
                   </div>
-                  <input type="text" className="bg-white w-full border p-2 rounded-md" />
+                  <div className="flex flex-col md:flex-row gap-2">
+                    <div className="flex-col gap-2 items-center">
+                      <label htmlFor="country">Country:</label>
+                      <input type="text" className="bg-white w-full border p-2 rounded-md" placeholder={`${user?.country}`} value={form.country} onChange={(e) => setForm({ ...form, country: e.target.value })} />
+                    </div>
+                    <div className="flex-col gap-2 items-center">
+                      <label htmlFor="zipCode">ZipCode:</label>
+                      <input type="number" className="bg-white w-full border p-2 rounded-md" placeholder={`${user?.zipCode}`} value={form.zipCode} onChange={(e) => setForm({ ...form, zipCode: e.target.value })} />
+                    </div>
+                  </div>
+                  <div className="flex flex-col md:flex-row gap-2">
+                    <div className="flex-col gap-2 items-center">
+                      <label htmlFor="education">Education:</label>
+                      <input type="text" className="bg-white w-full border p-2 rounded-md" placeholder={`${user?.education}`} value={form.education} onChange={(e) => setForm({ ...form, education: e.target.value })} />
+                    </div>
+                  </div>
+
 
 
                 </div>
-                <button className="mt-4 bg-[#206059] text-white px-4 py-2 rounded-md" onClick={handleUpdate}>
+                <button className="mt-4 bg-[#206059] text-white px-4 py-2 rounded-md" onClick={handleUpdateDetails}>
                   Save
                 </button>
               </form>
