dc7a2953f14e4109982bd9187942bc6c3bfbf68e
Umair Khan <mrumairkhan74@gmail.com>
2025-10-12 20:53:26 +0500

    Message Controller and Routes added

--- client/src/pages/Notification.jsx
+++ client/src/pages/Notification.jsx
@@ -26,11 +26,30 @@ const Notification = () => {
 
   return (
     <>
-      {notifications.map((notifi) => (
-        <h1 key={notifi._id}>
-          {notifi.sender.firstName} {notifi.sender.lastName}
-        </h1>
-      ))}
+      <div className="flex justify-between items-center bg-[#206059] w-full p-4 mt-2">
+        <h4 className="text-center text-2xl tracking-[3px]  font-[Poppins] text-white">
+          Notification
+        </h4>
+      </div>
+      <div className="container mx-auto">
+
+        {notifications.map((notify) => (
+          <div className="flex justify-between items-center gap-2 m-2 p-4 bg-gray-200 rounded-md">
+            <div className="flex items-center justify-center gap-2">
+              <img src={notify.sender?.profileImg?.url} alt="" className="w-15 h-15 rounded-full object-cover border border-[#206059] p-1" />
+              <div className="flex flex-col items-start ">
+                <h3 className="font-bold font-[Poppins]">{notify.sender?.firstName} {notify.sender?.lastName}</h3>
+                <p className="text-gray-600">{notify.message}</p>
+              </div>
+            </div>
+            <div className="">
+              <p className="text-[10px]">
+                {new Date(notify.createdAt).toDateString()}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
