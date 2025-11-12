ef7c93a708f0f06945718d5fdd97d109c8f2b003
Umair Khan <mrumairkhan74@gmail.com>
2025-11-12 13:31:02 +0500

    cookies adjust

--- client/src/ProtectedRoutes.jsx
+++ client/src/ProtectedRoutes.jsx
@@ -3,19 +3,24 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-    const { user, loading } = useSelector((state) => state.auth);
-    if (loading) return <div className="relative">
+  const { user, loading } = useSelector((state) => state.auth);
+
+  // Show loading until GetMe finishes
+  if (loading) {
+    return (
+      <div className="relative">
         <div className="absolute flex flex-col top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <img src="./favicon.png" alt="" className="w-20 h-18 object-cover" />
-            <h6 className="text-sm font-[Poppins]">Loading</h6>
+          <img src="./favicon.png" alt="" className="w-20 h-18 object-cover" />
+          <h6 className="text-sm font-[Poppins]">Loading</h6>
         </div>
-    </div>
-    if (!user)
-        // not logged in → kick back to login
-        return <Navigate to="/" replace />;
+      </div>
+    );
+  }
 
+  // Only redirect once loading is done
+  if (!user) return <Navigate to="/" replace />;
 
-    return children; // logged in → render page
+  return children;
 };
 
 export default ProtectedRoute;
