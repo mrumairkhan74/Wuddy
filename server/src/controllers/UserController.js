a747fe579cf4e6dfdec832f365ba5bb0943444c8
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2025-12-05 21:27:18 +0500

    login issue of token

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -130,18 +130,18 @@ const loginUser = async (req, res, next) => {
 
         res.cookie("token", AccessToken, {
             httpOnly: true,
-            //secure: process.env.NODE_ENV === "production", // true if on https
-            sameSite: "lax", // important for frontend <-> backend on different domains
-            //sameSite: "None", // important for frontend <-> backend on different domains
+            secure: process.env.NODE_ENV === "production", // true if on https
+            //sameSite: "lax", // important for frontend <-> backend on different domains
+            sameSite: "None", // important for frontend <-> backend on different domains
 
             maxAge: 24 * 60 * 1000, // 15 minutes (or whatever your access token expiry is)
         });
 
         res.cookie("refreshToken", RefreshToken, {
             httpOnly: true,
-            //secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
-            //sameSite: "None",
+            secure: process.env.NODE_ENV === "production",
+            //sameSite: "lax",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         return res.status(200).json({
