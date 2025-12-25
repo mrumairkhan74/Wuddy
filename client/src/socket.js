6bd68e5bad4ca4eba96e2706a82fd6c050c583ad
Umair Khan <mrumairkhan74@gmail.com>
2025-12-25 21:19:24 +0500

    socket api added

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Use your backend URL here
-const SOCKET_URL = import.meta.env.VITE_BACKEND_SOCKET_API;
+const SOCKET_URL = import.meta.env.VITE_BACKEND_API;
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
