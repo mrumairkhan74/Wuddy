c925710b9b1804b9b1c0788570b4168527a60572
Umair Khan <mrumairkhan74@gmail.com>
2025-12-25 20:58:53 +0500

    send messages real Time added

--- client/src/api/postsApi.js
+++ client/src/api/postsApi.js
@@ -5,37 +5,37 @@ const apiUrl = import.meta.env.VITE_BACKEND_API
 
 // createPost
 export const createPost = async (postData) => {
-    const res = await axios.post(`${apiUrl}/api/post/create`, postData, { withCredentials: true })
+    const res = await axios.post(`${apiUrl}/post/create`, postData, { withCredentials: true })
     return res.data
 }
 
 // getPosts
 export const getPost = async () => {
-    const res = await axios.get(`${apiUrl}/api/post/all`, { withCredentials: true })
+    const res = await axios.get(`${apiUrl}/post/all`, { withCredentials: true })
     return res.data.posts
 }
 
 // update Post
 export const updatePost = async (id, updated) => {
-    const res = await axios.put(`${apiUrl}/api/post/${id}`, updated, { withCredentials: true })
+    const res = await axios.put(`${apiUrl}/post/${id}`, updated, { withCredentials: true })
     return res.data
 }
 
 // deletePost
 
 export const deletePost = async (id) => {
-    const res = await axios.delete(`${apiUrl}/api/post/${id}`, { withCredentials: true })
+    const res = await axios.delete(`${apiUrl}/post/${id}`, { withCredentials: true })
     return res.data
 }
 // myPosts
 
 export const myPost = async () => {
-    const res = await axios.get(`${apiUrl}/api/post/myPosts`, { withCredentials: true })
+    const res = await axios.get(`${apiUrl}/post/myPosts`, { withCredentials: true })
     return res.data.posts
 }
 
 // gtePost by userId
 export const PostByUser = async (id) => {
-    const res = await axios.get(`${apiUrl}/api/post/user/${id}`, { withCredentials: true })
+    const res = await axios.get(`${apiUrl}/post/user/${id}`, { withCredentials: true })
     return res.data.posts
 }
